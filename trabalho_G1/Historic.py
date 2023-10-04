28e22f4e4354418cc6aac14a8246e43514b7b7fc
LucasGFeldmann <116355911+LucasGFeldmann@users.noreply.github.com>
2023-10-04 19:57:41 -0300

    Update Historic.py
    Versão para Entregar

--- trabalho_G1/Historic.py
+++ trabalho_G1/Historic.py
@@ -2,7 +2,7 @@ import Utilities
 from Utilities import cleanConsole
 
 stock = {}
-operationHistoric = [{'adicao': {'Caderno': {'quantia': 100, 'preco': 5.5, 'categoria': 'Material Escolar'}}}, {'adicao': {'Lapis': {'quantia': 50, 'preco': 1.5, 'categoria': 'Material Escolar'}}}, {'venda': {'Caderno': {'quantia': 10, 'preco': 5.5, 'total': 55.0, 'data': '10/04/23'}}}, {'venda': {'Caderno': {'quantia': 20, 'preco': 5.5, 'total': 110.0, 'data': '10/04/23'}}}, {'venda': {'Caderno': {'quantia': 30, 'preco': 5.5, 'total': 165.0, 'data': '10/04/23'}}}, {'venda': {'Caderno': {'quantia': 40, 'preco': 5.5, 'total': 220.0, 'data': '10/04/23'}}}, {'venda': {'Lapis': {'quantia': 25, 'preco': 1.5, 'total': 37.5, 'data': '10/04/23'}}}, {'venda': {'Lapis': {'quantia': 25, 'preco': 1.5, 'total': 37.5, 'data': '10/04/23'}}}]
+operationHistoric = []
 
 def showHistoric():
     for line in operationHistoric:
